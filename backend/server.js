9cfb1345baf5d19da458fe38c1461337ef0f8464
Abhishektharu <51353634+Abhishektharu@users.noreply.github.com>
2024-10-20 19:42:36 +0545

    send message final

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import authRoutes from './routes/authRoutes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
-
+import messageRoutes from './routes/messageRoutes.js'
 import dotenv from 'dotenv';
 
 dotenv.config(); 
@@ -14,6 +14,8 @@ app.use(express.json());  // For parsing JSON request bodies
 // Use the auth routes
 app.use('/api/auth', authRoutes);
 
+// use the /message routes
+app.use('/api/message', messageRoutes);
 // Start the server
 const PORT = process.env.PORT || 5000;
 
@@ -21,6 +23,6 @@ const uri = process.env.MONGO_URI;
 
 app.listen(PORT, () => {
     console.log(uri);
-    
+    connectToMongoDB;
     console.log(`Server running on port ${PORT}`);
 });
