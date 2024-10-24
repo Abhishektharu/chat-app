b23b29387c2c4e2746fe329085deebb392c1fed0
Abhishektharu <51353634+Abhishektharu@users.noreply.github.com>
2024-10-24 22:44:02 +0545

    search bar final

---
+++ frontend/src/hooks/useGetMessages.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import useConversation from "../zustand/useConversation";
+import toast from "react-hot-toast";
+
+const useGetMessages = () => {
+  const [loading, setLoading] = useState(false);
+  const { messages, setMessages, selectedConversation } = useConversation();
+
+  useEffect(() => {
+    const getMessages = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(`/api/messages/${selectedConversation._id}`);
+        const data = await res.json();
+        // console.log(data);
+
+        if (data.error) {
+          throw new Error(data.error);
+        }
+
+        //set the messages in zustand with ...messages all the prev messages and new msg => data
+        setMessages(data);
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+
+    };
+
+    if(selectedConversation?._id) getMessages();
+  }, [selectedConversation?._id, setMessages]);
+
+  return { messages, loading };
+};
+
+export default useGetMessages;
---
+++ frontend/src/hooks/useSendMessages.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import useConversation from "../zustand/useConversation";
+import toast from "react-hot-toast";
+const useSendMessages = () => {
+  const [loading, setLoading] = useState(false);
+  const { messages, setMessages, selectedConversation } = useConversation();
+
+  const sendMessage = async (message) => {
+    setLoading(true);
+    try {
+      const res = await fetch(
+        `/api/messages/send/${selectedConversation._id}`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ message }),
+        }
+      );
+      const data = await res.json();
+      console.log(data);
+
+      if (data.error) {
+        throw new Error(data.error);
+      }
+
+      //set the messages in zustand with ...messages all the prev messages and new msg => data
+      setMessages([...messages, data]);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { sendMessage, loading };
+};
+
+export default useSendMessages;
